dba6765f3445cc330f345ead459774c8f4bdf9d0
Daniel Pires <daniel.pires@devgo.org>
2023-01-12 08:54:25 -0100

    update cron job repetition time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('sync:products')->everyMinute();
+        $schedule->command('sync:products')->daily();
         // $schedule->command('inspire')->hourly();
     }
 
